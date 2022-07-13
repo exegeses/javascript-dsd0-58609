8a71d145ed19a75ce3f6699fca96669135d02f87
Marcos <exegeses@gmail.com>
2022-07-12 21:35:14 -0300

    mostrar  |  ocultar

---
+++ clase3/js/mostrarOcultar.js
@@ -0,0 +1,16 @@
+//ubicar elemento dentro del DOM
+let caja = document.getElementById('caja');
+
+//declaración de función/es de control
+function ocultar()
+{
+    //display:none;
+    //visibility:hidden
+    caja.style.display = 'none';
+}
+function mostrar()
+{
+    //display:block;
+    //visibility:visible
+    caja.style.display = 'block';    
+}
