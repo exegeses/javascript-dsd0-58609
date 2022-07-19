c91ad217f8974bd206fcb8a8808ea62a6db3df8e
Marcos <exegeses@gmail.com>
2022-07-19 19:26:53 -0300

    resolución tarea mostrar | ocultar

--- clase3/js/mostrarOcultar.js
+++ clase3/js/mostrarOcultar.js
@@ -1,5 +1,6 @@
 //ubicar elemento dentro del DOM
 let caja = document.getElementById('caja');
+let estado = 'visible';//flag  
 
 //declaración de función/es de control
 function ocultar()
@@ -7,10 +8,33 @@ function ocultar()
     //display:none;
     //visibility:hidden
     caja.style.display = 'none';
+    estado = 'oculto';
 }
 function mostrar()
 {
     //display:block;
     //visibility:visible
-    caja.style.display = 'block';    
+    caja.style.display = 'block';
+    estado = 'visible';
+}
+function mostrarOcultar()
+{
+    /*  Sergio
+    if( estado == 'oculto' ){
+        caja.style.display = 'block';
+        estado = 'visible';
+    }
+    else{
+        caja.style.display = 'none';
+        estado = 'oculto';
+    }*/
+    if( caja.style.display == 'none' ){
+        //caja.style.display = 'block';
+        mostrar();
+
+    }
+    else{
+        //caja.style.display = 'none';
+        ocultar();
+    }
 }
