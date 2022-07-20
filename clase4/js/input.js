aa937cab260f50a7fe7e8dd3b4b7b62ef4dc559c
Marcos <exegeses@gmail.com>
2022-07-19 21:46:47 -0300

    cuenta regresiva seteada por el usuario

--- clase4/js/input.js
+++ clase4/js/input.js
@@ -1,9 +1,63 @@
 // la caja input
-let nombre = document.getElementById('nombre');
+let fecha = document.getElementById('fecha');
 let txt = document.getElementById('txt');
+let txtDias = document.getElementById('txtDias');
+let txtHoras = document.getElementById('txtHoras');
+let txtMinutos = document.getElementById('txtMinutos');
+let txtSegundos = document.getElementById('txtSegundos');
+
 
 //el valor escrito en la caja input
 function verValor()
 {
-    txt.innerText = nombre.value;
+    txt.innerText = fecha.value;
+    let seleccionado = fecha.value;
+    /* substring( inicio, largo )*/
+    uAnio = seleccionado.substring( 0, 4 );
+    uMes = seleccionado.substring( 5, 7 )-1;
+    uDia = seleccionado.substring( 8, 10 );
+    let intervalo = setInterval( countdown, 1000 );
 }
+
+//declaramos función de control
+function dosDigitos( numero )
+{
+    if ( numero < 10 ){
+        numero = '0'+numero;
+    }
+    return numero;
+}
+
+function countdown()
+{
+    /* creamos dos objeto de fecha */
+    let ahora = new Date();
+    //let final = new Date( 2023, 0, 1 ); año nuevo
+    let final = new Date( uAnio, uMes, uDia );
+
+    /* conversión a unix timestamp
+        desde el 1º de enero de 1970 expresado en milisegundos  */
+    let actual = ahora.getTime();
+    let evento = final.getTime();
+    //diferencia entre fechas
+    let diferencia = evento - actual; // en milisegundos
+
+    /* obtenemos el tiempo en segundos, minutos, horas y dias */
+    let segundos = Math.trunc( diferencia/1000 );
+    let minutos =  Math.trunc( segundos/60 );
+    let horas = Math.trunc( minutos/60 );
+    let dias = Math.trunc( horas/24 );
+
+    /* obtenejmos los sobrantes con el módulo % */
+    horas = dosDigitos( horas%24 );
+    minutos = dosDigitos( minutos%60 );
+    segundos = dosDigitos( segundos%60 );
+
+    //imprimimos en el HTML
+    txtDias.innerText = dias;  
+    txtHoras.innerText = horas;      
+    txtMinutos.innerText = minutos;      
+    txtSegundos.innerText = segundos      
+}
+
+//countdown();
